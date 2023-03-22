41b99252be82611239d43ed6bd9d500c6295f1b4
Lolito-Lindsey-Official <87788112+Lolito-Lindsey-Official@users.noreply.github.com>
2023-03-22 03:42:01 -0500

    Added comments to the JS Code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/* Selects all elements that contain the class: ".panel" */
 const panels = document.querySelectorAll(".panel")
 
+/* Detects click when cursor is hovered over the photo */
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
@@ -7,6 +9,7 @@ panels.forEach(panel => {
     })
 })
 
+/* Used to remove active class from one photo to the next */
 function removeActiveClasses () {
     panels.forEach(panel => {
         panel.classList.remove('active')
